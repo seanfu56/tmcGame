d9a7ba2c0fa0cee2829b460eeac9d0cdb6e7c9df
Sean Fu <SeanFu920506@gmail.com>
2023-08-11 16:47:28 +0800

    add tab

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  let [game, setGame] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>台大傳醫讀書會遊戲</p>
-      </header>
-      <input placeholder="input password" />
+      <button
+        onClick={() => {
+          if (game === "") {
+            setGame("organ");
+          } else {
+            setGame("");
+          }
+        }}
+      >
+        game
+      </button>
+      {game === "" ? (
+        <header className="App-header">
+          <p>台大傳醫讀書會遊戲</p>
+        </header>
+      ) : (
+        <input placeholder="input password" />
+      )}
       {/* <Router>
         <Routes>
           <Route path="/organ" element={<p>六腑</p>} />
